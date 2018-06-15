7daf17ad299a508d7e704884903c7f3ae4ca71ae
Eric Crews <crews.eric@gmail.com>
2018-06-15 15:23:00 -0400

    Refactor webhook code

--- webhook.rb
+++ webhook.rb
@@ -1,17 +1,9 @@
 require 'sinatra'
 require 'twilio-ruby'
 
-get '/sms-quickstart' do
+post '/inbound' do
 	twiml = Twilio::TwiML::MessagingResponse.new do |r|
 		r.message(body: 'Ahoy! Thanks so much for your message.')
-	end
-
-	twiml.to_s
-end
-
-post '/facebook-inbound' do
-	twiml = Twilio::TwiML::MessagingResponse.new do |r|
-		r.message(body: 'Hello from Facebook!')
 	end
 
 	twiml.to_s
