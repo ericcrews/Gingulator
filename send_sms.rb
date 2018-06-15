b71d84f1bd02a8457bae9ff47f9a4043f4648542
Eric Crews <crews.eric@gmail.com>
2018-06-14 20:48:38 -0400

    Restore LIVE credentials

--- send_sms.rb
+++ send_sms.rb
@@ -1,7 +1,7 @@
 require 'twilio-ruby'
 
-account_sid = 'AC1f2e5d2314b5907e991c2763aad1f41f'
-auth_token = '6859bfa82b2d09ce19755547b18576af'
+account_sid = 'AC0c905be162720900a2afe2dd8014418f'
+auth_token = '6e79d9a036c11ecdac4b91e1a1672e07'
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
 from = '+13133074476' # Twilio number
@@ -10,5 +10,5 @@ to = ARGV[0] # Your mobile phone number (from the command line)
 client.messages.create(
 	from: from,
 	to: to,
-	body: "Hello, world!"
+	body: 'Hello, world!'
 )
