061ec32b6b1c8a173ce38e9add23131af0fa037f
Eric Crews <crews.eric@gmail.com>
2018-06-21 19:03:52 -0400

    Add Slack webhook

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.5.1'
 
 # Twilio SMS
 gem 'twilio-ruby'
+# Easier HTTP Requests
+gem 'httparty'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use Puma as the app server
--- app/controllers/listeners_controller.rb
+++ app/controllers/listeners_controller.rb
@@ -1,9 +1,34 @@
 require 'twilio-ruby'
+require 'httparty'
 
 class ListenersController < ApplicationController
   def slack
-		puts 'slack - OK'
-  end
+		event_wrapper = request.request_parameters
+
+		# Slack URL Verification
+		if event_wrapper['challenge']
+			render json: event_wrapper['challenge']
+		end
+
+		# Process Slack Event
+		event = event_wrapper['event']
+		unless event['username']
+			text = 'Hello, world!'
+
+			HTTParty.post(
+				'https://slack.com/api/chat.postMessage',
+				headers: {
+					'Content-Type' => 'application/json',
+					'Authorization' => 'Bearer ' \
+					"#{Rails.application.credentials.slack[:bot_token]}"
+				},
+				body: {
+					'channel' => event['channel'],
+					'text' => text
+				}.to_json
+			)
+		end
+	end
 
   def twilio
 		twiml = Twilio::TwiML::MessagingResponse.new do |r|
