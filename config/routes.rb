fb0f67cad78ee2fcaf0712de91672d05ee76a5c5
Caleb Latimer <nohitokaze@gmail.com>
2018-07-12 17:24:49 -0400

    Fix again

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :home, :app_infos
-  root to: 'home#home'
+  root to: 'home#index'
   post '/' => 'app_infos#data'
 end
