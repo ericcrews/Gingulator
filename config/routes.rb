00f55bf8f0926d129fc26f7542047b695faa8f71
Caleb Latimer <nohitokaze@gmail.com>
2018-07-12 17:12:59 -0400

    Route Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
-  resources :home :app_infos
+  resources :home, :app_infos
   root to: 'home#home'
   post '/' => 'app_infos#data'
 end
