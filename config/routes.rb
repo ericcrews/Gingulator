b8d8a79e6a3277df6b162925444b161760864480
Caleb Latimer <nohitokaze@gmail.com>
2018-07-12 16:17:28 -0400

    Updated route, added home-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :app_infos
-  root to: 'home'
+  root to: 'app_infos#data'
   post '/' => 'app_infos#data'
 end
