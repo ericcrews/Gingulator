d10669fe64afa82b1b540e3bda9f6db6e09c7e81
Eric Crews <crews.eric@gmail.com>
2018-07-12 15:36:40 -0400

    Bypass authentication for Dialogflow

--- app/controllers/app_infos_controller.rb
+++ app/controllers/app_infos_controller.rb
@@ -3,6 +3,10 @@ require 'json'
 class AppInfosController < ApplicationController
   before_action :set_app_info, only: [:show, :update, :destroy]
 
+  # This allows Dialogflow to post to the Gingulator without authentication.
+  # Probably not a good idea to do this in an actual app.
+  skip_before_action :authenticate_user!, only: [:data]
+
   # GET /app_infos
   def index
     @app_infos = AppInfo.all
