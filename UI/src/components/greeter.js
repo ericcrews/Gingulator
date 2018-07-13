e5f25275bf998cf121f97e1ace6608bc9d48d693
Caleb Latimer <nohitokaze@gmail.com>
2018-07-13 09:45:19 -0400

    Syncing Bedginghorse code to UI branch

--- UI/src/components/greeter.js
+++ UI/src/components/greeter.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import ExitToApp from '@material-ui/icons/ExitToApp'
 import '../App.css'
 const Intro = () => {
       let prompts = {
             m1: "Welcome to the Gingulator!",
             m2: "Sam's a busy guy but if you require assistance the Gingulator is happy to be of service",
-            m3: "Please procure the closure of your query below"
+            m3: "Try out the general view below or login for extended functionality"
       }
             return(
                   <div className="App-intro">
                         <p>{prompts.m1}</p>
                         <p>{prompts.m2}</p>
                         <p>{prompts.m3}</p>
+                        <Button variant="contained"
+                                    className ="login-button"
+                                    href='https://gingulator.herokuapp.com/login'><ExitToApp /> Login</Button>
                   </div>
             )
       }
